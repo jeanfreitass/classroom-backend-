2b5f6aec9113412760638267725654f817ca442e
Jean Freitas <jean.freitas@iesb.edu.br>
2025-10-17 10:45:16 -0300

    Finalizando Aula 10

--- aulas/aula10/app.js
+++ aulas/aula10/app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 const tarefasRouter = require ('./routes/tarefasRouter');
 
-const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOSS}/`;
+const url = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PSSWD}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_DB_NAME}`;
 
 mongoose
   .connect(url)
--- aulas/aula10/tests/tarefasRouter.test.js
+++ aulas/aula10/tests/tarefasRouter.test.js
@@ -12,6 +12,8 @@ describe('Teste do recurso /tarefas', () =>{
         ({nome: "Estudar"});
         expect(response.status).toBe(201);
         expect(response.body.id).toBeDefined();
+        expect(response.body.nome).toBe("Estudar");
+        expect(response.body.concluida).toBe(false);
         id = response.body.id;
     });
 
@@ -27,16 +29,32 @@ describe('Teste do recurso /tarefas', () =>{
         expect(response.body.id).toBeDefined();
     });
 
+    test('GET / id deve retornar 404', async() =>{
+        const response = await request.get(`${url}/000000000000000000000000`);
+        expect(response.status).toBe(404);
+        expect(response.body.msg).toBe("Tarefa não encontrada");
+        });
+
     test('PUT / id deve retornar 200', async() =>{
         const response = await request.put(`${url}/${id}`).send
         ({nome: "Estudar REST", concluida: true});
         expect(response.status).toBe(200);
         expect(response.body.id).toBeDefined();
     });
+    test('PUT / id deve retornar 404', async() =>{
+        const response = await request.put(`${url}/000000000000000000000000`);
+        expect(response.status).toBe(404);
+        expect(response.body.msg).toBe("Tarefa não encontrada");
+        });
 
     test('DELETE / id deve retornar 204', async() =>{
         const response = await request.delete(`${url}/${id}`);
         expect(response.status).toBe(204);
     });
+    test('DELETE / id deve retornar 404', async() =>{
+        const response = await request.delete(`${url}/000000000000000000000000`);
+        expect(response.status).toBe(404);
+        expect(response.body.msg).toBe("Tarefa não encontrada");
+        });
 
 });
